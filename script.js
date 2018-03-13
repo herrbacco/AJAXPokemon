d931c24d31657a644cf7a7dd8db7af32afd6027c
herrbacco <herrbacco@gmail.com>
2018-03-13 16:22:26 -0700

    added sidebar nav and jump to details from there

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function(){
 
-$('#button');
+
+  var $sidenav = $('.sidenav');
   var $display = $('#displayDiv');
   var $start = $('#start');
   var $stop =$('#stop');
@@ -9,6 +10,57 @@ $('#button');
 
   var index = 0;
   var cards = [];
+
+  var loadSideNav = function(){
+    $.ajax({
+      type:'GET',
+      url: 'https://api.pokemontcg.io/v1/cards',
+      data: cards,
+      success: function(cards){
+
+        for(let z =0; z<cards.cards.length; z++)
+        {
+          $sidenav.append('<div id="'+z+'">'+cards.cards[z].name+'</div>');
+        }
+
+        console.log("LoadSideNav");
+      },
+      error: function(e){
+        alert('error getting pokemon cards for side nav: '+ e);
+      }
+    });
+  }
+  loadSideNav();
+
+  function loadPokemon(data){
+    var whichPokemon = ($(this).attr('id') ? $(this).attr('id') : data.data.ind);
+
+    console.log(whichPokemon);
+
+    $stop.click();
+    $.ajax({
+      type:'GET',
+      url: 'https://api.pokemontcg.io/v1/cards',
+      data: cards,
+      success: function(cards){
+
+        $display.empty();
+
+        $display.append('<h2>'+cards.cards[whichPokemon].name+"</h2>");
+        $display.append('<img src="'+cards.cards[whichPokemon].imageUrl+'">');
+        $display.append('<h3>Art by: '+cards.cards[whichPokemon].artist+'</h3>');
+
+        console.log("LoadPokemon");
+        index = (parseInt(whichPokemon) >= 99 ? 0 : parseInt(whichPokemon)+1);
+      },
+      error: function(){
+        alert('error getting pokemon cards:LoadPokemon');
+      }
+    });
+  }
+
+
+  $sidenav.on('click','div',loadPokemon);
 
   $start.on('click',function(){
     console.log('start clicked');
@@ -31,22 +83,18 @@ $('#button');
       data: cards,
       success: function(cards){
 
-        var $nameH2 = '<h2>'+cards.cards[index].name+"</h2>";
-        var $picIMG = '<img src="'+cards.cards[index].imageUrl+'">';
-        var $textH3 = '<h3>Art by: '+cards.cards[index].artist+'</h3>';
 
         $display.empty();
+        console.log(index);
+        $display.append('<h2>'+cards.cards[index].name+"</h2>");
+        $display.append('<img src="'+cards.cards[index].imageUrl+'">');
+        $display.append('<h3>Art by: '+cards.cards[index].artist+'</h3>');
 
-        $display.append($nameH2);
-        $display.append($picIMG);
-        $display.append($textH3);
-
-        console.log("cards retrieved");
-
-        index = (index >= 99 ? 0:index+1);
+        console.log("LoadNextPokemon");
+        index = (index >= 99 ? 0 :index+1);
       },
       error: function(){
-        alert('error getting pokemon cards');
+        alert('error getting pokemon cards: loadNextPokemon');
       }
     })
   };
